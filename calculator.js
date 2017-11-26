6056d0ee6023abe4f1ae54f5d4a64a3dc0bbd642
Tim Tolley <Tim@Tims-MBP.fritz.box>
2017-11-26 21:47:56 +1300

    coded and designed calculator

--- calculator.js
+++ calculator.js
@@ -1,28 +1,54 @@
-let display="0";
+let display="";
+let memory="";
 
 
 //add digit to display
+
+
 function addDigit(digit){
-  if((display=="0") && (display.includes(".")==false)){
+  if((display=="") && (memory="")&& (display.includes(".")==false)){
     display=digit;
+  } else if ((display=="")&&(memory!=="")){
+    display=memory+digit;
+    memory="";
   } else {
     display=display+digit;
+
   }
   document.getElementById("display").value=display;
-}
 
+}
 
 //clear Entry
 function remove(){
-  if(display!=="0"){
+    display=memory;
+  document.getElementById("display").value=display;
+}
+
+//clear memory
+function removeAll(){
+  if(display!==""){
     display="";
   }
   document.getElementById("display").value=display;
 }
 
 function calculate(){
-  let result= eval(display);
+    let result= eval(display);
   display=display+"="+result;
   document.getElementById("display").value=display;
+  memory=result;
+}
 
+function neg(){
+  if (display=""){
+    display="-";
+  }
+  if(display.charAt(0)!=="-"){
+    display="-"+display;
+  }
+  else{
+    display=display.substr(1, display.length);
+  }
+document.getElementById("display").value=display;
 }
